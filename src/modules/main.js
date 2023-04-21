e31634cffc9febddf6a23153df478fae92a9ac22
Vitalii <tomrain78@gmail.com>
2023-04-21 13:19:06 +0300

    working with data

--- src/modules/main.js
+++ src/modules/main.js
@@ -4,6 +4,9 @@ const main = () => {
     const taskList = document.querySelector("#task-list");
     const emptyList = document.querySelector("#empty-list");
 
+    let tasks = [];
+    checkEmptyList();
+
     form.addEventListener("submit", addTask);
     taskList.addEventListener("click", deleteTask);
     taskList.addEventListener("click", doneTask);
@@ -13,9 +16,19 @@ const main = () => {
 
         const taskText = taskInput.value;
 
+        const newTask = {
+            id: Date.now(),
+            text: taskText,
+            done: false,
+        };
+
+        tasks.push(newTask);
+
+        const cssClass = newTask.done ? "task-title done" : "task-title";
+
         const taskHTML = `
-        <li class="list-group-item">
-                <span class="task-title">${taskText}</span>
+        <li id="${newTask.id}" class="list-group-item">
+                <span class="${cssClass}">${newTask.text}</span>
             <div>
                 <button type="button" data-action="done" class="btn-done item-buttons">Done</button>
                 <button type="button" data-action="delete" class="btn-delete item-buttons">Delete</button>
@@ -27,9 +40,7 @@ const main = () => {
         taskInput.value = "";
         taskInput.focus();
 
-        if (taskList.children.length > 1) {
-            emptyList.classList.add("none");
-        }
+        checkEmptyList();
     }
 
     function deleteTask(e) {
@@ -38,11 +49,13 @@ const main = () => {
         }
 
         const parenNode = e.target.closest(".list-group-item");
-        parenNode.remove();
-        if (taskList.children.length === 1) {
-            emptyList.classList.remove("none");
-        }
+        const id = Number(parenNode.id);
+        
+        tasks = tasks.filter((task) => task.id !== id);
 
+        parenNode.remove();
+
+        checkEmptyList();
     }
 
     function doneTask(e) {
@@ -51,9 +64,30 @@ const main = () => {
         }
 
         const parenNode = e.target.closest(".list-group-item");
+
+        const id = Number(parenNode.id);
+        const task = tasks.find((task) => task.id === id);
+        task.done = !task.done
+
         const taskTitle = parenNode.querySelector(".task-title");
         taskTitle.classList.toggle("done");
 
+    }
+
+    function checkEmptyList() {
+        if (tasks.length === 0) {
+            const emptyListHTML = `<li class="list-group-item">
+            <div id="empty-list" class="empty-text">To-do list is empty</div>
+        </li>`;
+
+        taskList.insertAdjacentHTML('afterbegin', emptyListHTML);
+        }
+
+        if (tasks.length > 0) {
+            const emptyListElement = document.querySelector('#empty-list');
+            emptyListElement ? emptyListElement.remove() : null;
+
+        }
     }
 };
 
